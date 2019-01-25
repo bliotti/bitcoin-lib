b1a9af0ecea8f534a2757f758bbd84db3d7a020f
Brian Liotti <brianliotti@Brians-MacBook-Pro.local>
2019-01-25 03:30:01 -0500

    init

---
+++ ecc-tests.py
@@ -0,0 +1,8 @@
+
+from tests import FieldElementTest
+from helper import run
+
+run(FieldElementTest("test_ne"))
+run(FieldElementTest("test_sub"))
+run(FieldElementTest("test_mul"))
+run(FieldElementTest("test_div"))
